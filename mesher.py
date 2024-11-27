0e9a11721a5dc51049d766e735df88322da1e2a9
Varun Unnithan <varun.unnithan33@gmail.com>
2024-11-27 15:28:15 -0500

    point starts

--- mesher.py
+++ mesher.py
@@ -19,6 +19,7 @@ z > a*x^4+c*x^2-f*y + g*abs(x)+(y+q*x**2+s*abs(x))*(h*x**2-i*y+j)
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
 import pyvista as pv
 
 # Define parameters (you can adjust these as needed)
@@ -31,3 +32,47 @@ i = 0.45
 j = 0.17
 q = 0
 s = 1.84
+
+points = []
+faces = []
+distances_to_edges = []
+
+# top surface
+def top_z(x, y):
+    return a * x**4 + c * x**2 - f * y + g * abs(x)
+
+# bottom surface, characterized by the `term` variable which models the z difference
+def bottom_z(x, y):
+    term = (y + q * x**2 + s * abs(x)) * (h * x**2 - i * y + j)
+    return top_z(x, y) + term
+
+# Make the approximation that the gradients of the top surface approximate the bottom faces
+# You can use gradients in the meshing process to assign more cells to regions of high curvature, but
+# in this model, regions of high gradients tend to be near the edges of the surface, so as long as we 
+# mesh the edges well, we should be fine.
+def gradient_top(x, y):
+    return np.array([4 * a * x**3 + 2 * c * x - g * np.sign(x), -f])
+
+def gradient_bottom(x, y):
+    return np.array([4 * a * x**3 + 2 * c * x - g * np.sign(x) + 2*y*h*x + 4*q*h*x**3 - 2*q*i*y*x + 2*q*j*x + 3*s*h*x**2 + s*j,
+                     -f + h*x**2 - 2*i*y + j - q*i*x**2 + s*i*x])
+
+
+def solve_for_x(y, q, s):
+    """
+    Solve y = -q*x^2 - s*|x| for x >= 0.
+    Returns the positive solution.
+    """
+    # y = -q*x^2 - s*x => q*x^2 + s*x + y = 0
+    # Using quadratic formula
+    discriminant = s**2 - 4*q*y
+    if discriminant < 0:
+        return None  # No real solution
+    
+    # handle abs value case and parabola case
+    if q == 0:
+        x = -y / s
+    else:
+        # quadratic formula
+        x = (-s + np.sqrt(discriminant)) / (2*q)
+    return x
